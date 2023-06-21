6ab8f752aa74e7bae5d849c0fff0c21ee1f3e8a5
fg <2646921352@qq.com>
2023-06-21 11:15:40 +0800

    第三次提交， 一个类

---
+++ src/main/java/com/component/Computer.java
@@ -0,0 +1,15 @@
+package com.component;
+
+/**
+ * @Author: fg
+ * @Time: 2023-06-21 11:14
+ * @function:
+ */
+public class Computer {
+
+    public static void main(String[] args) {
+
+        System.out.println("hehhahHHAFH");
+    }
+
+}
